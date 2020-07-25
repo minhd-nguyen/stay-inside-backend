a658d3dbe06a23c3019278723c7d14bdb9fff70c
zbarovsky <zachary.barovsky@gmail.com>
2020-07-25 13:07:09 -0700

    rough stub for post(create) event route

--- routes/events.js
+++ routes/events.js
@@ -1,11 +1,27 @@
 require('dotenv').config()
 const express = require('express')
 const router = express.Router()
+//const db = require('../models')
 
 const Event = require('../models/Event')
+const User = require('../models/User')
 
 // API Routes
 // TODO: GET -- post events to home page
+
 // TODO: POST -- add created event to db
+router.post('/create', (req, res) => {
+    // get event created by specific user
+    // add event to db and register it to be with a specific user
+    Event.create(req.body)
+        .then(newEvent => {
+            res.send(newEvent)
+        }).catch(error => {
+            res.send({message: 'Server error'})
+            console.error(error)
+        })
+})
+
 // TODO: PUT -- edit said event
+
 // TODO: DELETE -- delete said event
