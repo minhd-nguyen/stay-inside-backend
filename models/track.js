49d9ba4056d83795de76f2bba36f6f844b306479
nguyendm77 <nguyendm77@gmail.com>
2020-07-26 18:47:52 -0700

    fixed conflict

---
+++ models/track.js
@@ -0,0 +1,26 @@
+// Track Schema
+// playlistID: integer, comment [ref: comment]
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const TrackSchema = new Schema ({
+    
+    name: {
+        type: String,
+    },
+    artist: {
+        type: String
+    },
+    album: {
+        type: String
+    },
+    playlistId: {
+        type: Number
+    },
+    // playlistID goes here type: Int ref
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'comments'
+    }]
+})
+
+module.exports = Event = mongoose.model('events', EventSchema)
---
+++ routes/tracks.js
@@ -0,0 +1,96 @@
+require('dotenv').config()
+const express = require('express')
+const router = express.Router()
+const Spotify = require('node-spotify-api')
+const { search } = require('../routes')
+
+// create a API Spotify Client
+let spotify = new Spotify({
+    id: process.env.CLIENT_ID,
+    secret: process.env.CLIENT_SECRET
+});
+
+//Store the results of a request to spotify
+let results = [];
+
+// GET ROUTE
+router.post('/', function(req, res) {    
+    //Get the type of Query from the User
+    let type = req.body.param_type;
+    // console.log(type);
+    // console.log('🏆');
+
+    //Get the query from the user
+    let query = req.body.param_query;
+    //Clear out old results
+    results = [];
+    //Make a request to Spotify
+    spotify.search({type: type, query: query})
+        .then(function (spotRes) {
+        // console.log(spotRes[type+'s']);
+        // console.log('🥇');
+
+            // Store the artist, song, preview link, and album in the results array
+            spotRes[type+'s'].items.forEach(function(ea){
+                // console.log(ea.id);
+                // console.log('🎲');
+                if (type == 'album') {
+                    // console.log(ea.id);
+                    // console.log('🎲');
+                    results.push({id: ea.id,
+                                uri: ea.uri,
+                                image: ea.images[0],
+                                name: ea.name,
+                                artist: ea.artists[0].name,
+                                // link: ea.external_urls.spotify
+                    });
+                } else if (type == 'artist') {
+                    // console.log(ea.id);
+                    // console.log('🤹🏻‍♂️');
+                    results.push({id: ea.id,
+                                uri: ea.uri,
+                                image: ea.images[0],
+                                name: ea.name,
+                                // link: ea.external_urls.spotify
+                    });
+                } else if (type == 'track') {
+                    // console.log(ea.preview_url);
+                    // console.log(ea.id);
+                    // console.log('⚓️');
+                    results.push({id: ea.id,
+                                uri: ea.uri,
+                                name:ea.name,
+                                artist: ea.artists[0].name,
+                                album: ea.album.name,
+                                // link: ea.external_urls.spotify,
+                                preview: ea.preview_url
+                    });
+                } 
+            });
+            // Return results to the view
+            // console.log(results);
+            // console.log('🎳');
+            res.render('/search', {results: results});
+        })
+        .catch(function (err) {
+            console.log(err);
+            throw err;
+        });
+    
+});
+
+// View tracks from album
+router.get('/:id', (req, res) => {
+    spotify
+    .request('https://api.spotify.com/v1/albums/'+ req.params.id + '/tracks')
+    .then(function(data) {
+    //   console.log(data); 
+    //   console.log('🎡')
+      res.render('playlists/tracks', {results: data.items})
+    })
+    .catch(function(err) {
+    console.error('Error occurred: ' + err); 
+    })
+});
+
+module.exports = router
